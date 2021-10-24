fd28c6d5cb68931ed7adc63f9fadfe04777fb904
ANKUR SINHA <ankursinha03@gmail.com>
2021-10-24 23:45:19 +0530

    Added: Proof of Work

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const BlockChain = require('./res/blockChain');
-const Block = require('./res/block');
+const BlockChain = require("./res/blockChain");
+const Block = require("./res/block");
 
-let patoCoin = new BlockChain();
-patoCoin.addBlock(new Block(1, "26-09-2021/12:30AM", { wallet: 15 }));
-patoCoin.addBlock(new Block(2, "26-09-2021/12:30AM", { wallet: 15 }));
-patoCoin.addBlock(new Block(3, "26-09-2021/12:30AM", { wallet: 15 }));
-
+let patoCoin = new BlockChain(4);
+console.log("Mining: 1");
+patoCoin.addBlock(new Block(1, new Date().toLocaleString(), { wallet: 15 }));
+console.log("Minted: 1");
+console.log("Mining: 2");
+patoCoin.addBlock(new Block(2, new Date().toLocaleString(), { wallet: 15 }));
+console.log("Minted: 2");
+console.log("Mining: 3");
+patoCoin.addBlock(new Block(3, new Date().toLocaleString(), { wallet: 15 }));
+console.log("Minted: 3");
 
 console.log(patoCoin);
-console.log(patoCoin.validChain())
-patoCoin.chain[2].data = { wallet: 50 };
-console.log(patoCoin.validChain())
+console.log(patoCoin.validChain());
+// patoCoin.chain[2].data = { wallet: 50 };
+// console.log(patoCoin.validChain())
--- res/block.js
+++ res/block.js
@@ -1,16 +1,29 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
 class Block {
-    constructor(index, timeStamp, data, prevHash = '') {
-        this.index = index;
-        this.timeStamp = timeStamp;
-        this.data = data;
-        this.prevHash = prevHash;
-        this.hash = this.computeHash();
+  constructor(index, timeStamp, data, prevHash = "") {
+    this.index = index;
+    this.timeStamp = timeStamp;
+    this.data = data;
+    this.prevHash = prevHash;
+    this.hash = this.computeHash();
+    this.nonce = 0;
+  }
+  computeHash() {
+    return SHA256(
+      this.index +
+        this.timeStamp +
+        JSON.stringify(this.data) +
+        this.prevHash +
+        this.nonce
+    ).toString();
+  }
+  mineBlock(diff) {
+    while (this.hash.substring(0, diff) !== Array(diff + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.computeHash();
     }
-    computeHash() {
-        return SHA256(this.index + this.timeStamp + JSON.stringify(this.data) + this.prevHash).toString();
-    }
+  }
 }
 
 module.exports = Block;
